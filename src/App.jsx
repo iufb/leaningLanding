903c3cc1d698448b3fa71de208a2b204b2fcafe8
iufb <bashirov3ld2@gmail.com>
2025-04-24 10:35:49 +0500

    feat:shadcn, font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Button } from '@/shared/ui/button'
 import { FormattedMessage } from 'react-intl'
 import './App.css'
 
 function App() {
 
     return (
-        <h1 className='text-4xl text-red-400'> <FormattedMessage id='test' /></h1>
+        <>
+            <h1 className='text-4xl text-red-400'> <FormattedMessage id='test' /></h1>
+            <Button> helolo</Button>
+        </>
     )
 }
 
